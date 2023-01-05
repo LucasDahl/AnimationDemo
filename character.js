07db75bdc4a8b75b98de30c652d240e0a9f8dec8
Lucas Dahl <lucasdahl425@yahoo.com>
2023-01-04 22:27:59 -0800

    Moving animations
    - Took out the spinning animation
    - Made all the animations move

--- character.js
+++ character.js
@@ -6,12 +6,14 @@ class Character {
 
         // Set the game engine
         this.game = game;
+        this.speed = 50;
+        this.rightX = 0;
+        this.leftX = 562
+        this.backY = 562
+        this.frontY = 32;
 
         // Get the spriteshhett
         this.spritesheet = ASSET_MANAGER.getAsset("./sprites/MaleCasual.png");
-
-        // Set the animator
-        //this.animator = new Animator(this.spritesheet, 32, 32, 32, 32, 4, 0.2);
 
         // Get the animations
         this.animations = [];
@@ -21,11 +23,11 @@ class Character {
     // This method will get all the animations.
     getAnimations() {
 
-        // Spinning animation
-        this.animations[0] =  new Animator(this.spritesheet, 0, 0, 32, 32, 4, 0.2);
-
         // Walking right animation
-        this.animations[1] =  new Animator(this.spritesheet, 0, 32, 32, 32, 7, 0.2);
+        this.animations[0] =  new Animator(this.spritesheet, 0, 32, 32, 32, 7, 0.2);
+
+        // Walking left animation
+        this.animations[1] =  new Animator(this.spritesheet, 0, 128, 32, 32, 7, 0.2);
 
         // Walking back animation
         this.animations[2] =  new Animator(this.spritesheet, 0, 64, 32, 32, 7, 0.2);
@@ -33,27 +35,43 @@ class Character {
         // Walking front animation
         this.animations[3] =  new Animator(this.spritesheet, 0, 96, 32, 32, 7, 0.2);
 
-        // Walking left animation
-        this.animations[4] =  new Animator(this.spritesheet, 0, 128, 32, 32, 7, 0.2);
     }
 
     // This is the update method called on each frame.
     update() {
 
+        // Update the x or y value
+        this.rightX += this.speed * this.game.clockTick;
+        this.leftX -= this.speed * this.game.clockTick;
+        this.backY -= this.speed * this.game.clockTick;
+        this.frontY += this.speed * this.game.clockTick;
+
+        // Reset the x or values
+        if(this.rightX > 562) {
+            this.rightX = 0;
+        }
+
+        if(this.leftX < 0) {
+            this.leftX = 562;
+        }
+
+        if(this.backY < 0) {
+            this.backY = 562;
+        }
+
+        if(this.frontY > 562) {
+            this.frontY = 32;
+        }
+
     };
 
-    // This method will draw teh Character.
+    // This method will draw the Character.
     draw(ctx) {
 
-        // Properties
-        const x = 25;
-        var y = 25;
-
         // Draw the animations
-        for(var i = 0; i < this.animations.length; i++) {
-            this.animations[i].drawFrame(this.game.clockTick, ctx, x, y);
-            y += 32;
-        }
+        this.animations[0].drawFrame(this.game.clockTick, ctx, this.rightX, 0);
+        this.animations[1].drawFrame(this.game.clockTick, ctx, this.leftX, 32);
+        this.animations[2].drawFrame(this.game.clockTick, ctx, 0, this.backY);
+        this.animations[3].drawFrame(this.game.clockTick, ctx, 32, this.frontY);
     };
-
 }
