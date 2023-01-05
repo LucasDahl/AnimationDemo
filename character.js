4f0f7d695a9dd45172d91d7507d5a75c53c52fe7
Lucas Dahl <lucasdahl425@yahoo.com>
2023-01-05 13:32:55 -0800

    Player movement
    - Allowed for player movement

--- character.js
+++ character.js
@@ -4,16 +4,16 @@ class Character {
     // This is the constructor for the character
     constructor(game) {
 
-        // Set the game engine
+        // Properties
         this.game = game;
         this.speed = 50;
-        this.rightX = 0;
-        this.leftX = 562
-        this.backY = 562
-        this.frontY = 32;
+        this.x = 0;
+        this.y = 0;
+        this.index = 6;
 
         // Get the spriteshhett
         this.spritesheet = ASSET_MANAGER.getAsset("./sprites/MaleCasual.png");
+        this.animator = new Animator(this.spritesheet, 0, 32, 32, 32, 7, 0.2);
 
         // Get the animations
         this.animations = [];
@@ -35,43 +35,56 @@ class Character {
         // Walking front animation
         this.animations[3] =  new Animator(this.spritesheet, 0, 96, 32, 32, 7, 0.2);
 
+        // Idle left
+        this.animations[4] =  new Animator(this.spritesheet, 96, 0, 32, 32, 1, 100000);
+
+        // Idle right
+        this.animations[5] =  new Animator(this.spritesheet, 64, 0, 32, 32, 1, 100000);
+
+        // Idle front
+        this.animations[6] =  new Animator(this.spritesheet, 0, 0, 32, 32, 1, 100000);
+
+        // Idle back
+        this.animations[7] =  new Animator(this.spritesheet, 32, 0, 32, 32, 1, 100000);
+
     }
 
     // This is the update method called on each frame.
     update() {
 
-        // Update the x or y value
-        this.rightX += this.speed * this.game.clockTick;
-        this.leftX -= this.speed * this.game.clockTick;
-        this.backY -= this.speed * this.game.clockTick;
-        this.frontY += this.speed * this.game.clockTick;
 
-        // Reset the x or values
-        if(this.rightX > 562) {
-            this.rightX = 0;
+        // Update based on player movement.
+        if (this.game.keys["d"] || this.game.keys["D"]) {
+            this.x += this.speed * this.game.clockTick;
+            this.index = 0;
+        } else if (this.game.keys["a"] || this.game.keys["A"]) {
+            this.x -= this.speed * this.game.clockTick;
+            this.index = 1;
+        } else if (this.game.keys["w"] || this.game.keys["W"]) {
+            this.y -= this.speed * this.game.clockTick;
+            this.index = 2;
+        } else if (this.game.keys["s"] || this.game.keys["S"]) {
+            this.y += this.speed * this.game.clockTick;
+            this.index = 3;
+        } else {
+
+            // If the player is not pressing a key
+            if(this.index == 0) {
+                this.index = 5;
+            } else if(this.index == 1) {
+                this.index = 4;
+            } else if(this.index == 2) {
+                this.index = 7;
+            } else if(this.index == 3) {
+                this.index = 6;
+            } 
         }
-
-        if(this.leftX < 0) {
-            this.leftX = 562;
-        }
-
-        if(this.backY < 0) {
-            this.backY = 562;
-        }
-
-        if(this.frontY > 562) {
-            this.frontY = 32;
-        }
-
     };
 
     // This method will draw the Character.
     draw(ctx) {
 
         // Draw the animations
-        this.animations[0].drawFrame(this.game.clockTick, ctx, this.rightX, 0);
-        this.animations[1].drawFrame(this.game.clockTick, ctx, this.leftX, 32);
-        this.animations[2].drawFrame(this.game.clockTick, ctx, 0, this.backY);
-        this.animations[3].drawFrame(this.game.clockTick, ctx, 32, this.frontY);
+        this.animations[this.index].drawFrame(this.game.clockTick, ctx, this.x, this.y);
     };
 }
